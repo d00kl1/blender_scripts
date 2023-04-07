dd387ada4d6d02a02b1f9125d4580f55ad29363e
Harikrishnan Varma <harikrishnan_v@yahoo.com>
2023-04-07 14:02:18 +0530

    Remove unnecessary print

--- name_mesh_to_node_name.py
+++ name_mesh_to_node_name.py
@@ -6,7 +6,6 @@ collection_name = 'south_east'
 for collection in bpy.data.collections:    
     if collection.name == collection_name:
         for obj in collection.all_objects:
-            print(obj.type)
             if (obj.type == 'MESH') or (obj.type == 'CURVE'):
                 if (obj.data.name != obj.name) and (obj.data.users == 1):
                     print(f'Renaming {obj.data.name} -> {obj.name}')
