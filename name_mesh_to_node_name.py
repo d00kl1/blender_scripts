b0924ec89ff3c2befb77da0db9752a7967e52401
Harikrishnan Varma <harikrishnan_v@yahoo.com>
2023-04-07 16:28:22 +0530

    Add prefix

--- name_mesh_to_node_name.py
+++ name_mesh_to_node_name.py
@@ -1,11 +1,17 @@
 # rename mesh name to object name
 import bpy
 
-collection_name = 'south_east'
+collection_name = 'south'
+
+obj_name_prefix = 's_'
 
 for collection in bpy.data.collections:    
     if collection.name == collection_name:
         for obj in collection.all_objects:
+            if not obj.name.startswith(obj_name_prefix):
+                print(f'Renaming {obj.name} -> {obj_name_prefix}{obj.name}')
+                obj.name = obj_name_prefix + obj.name                
+                
             if (obj.type == 'MESH') or (obj.type == 'CURVE'):
                 if (obj.data.name != obj.name) and (obj.data.users == 1):
                     print(f'Renaming {obj.data.name} -> {obj.name}')
